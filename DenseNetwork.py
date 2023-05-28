1d93ecadd04b09627236555dcf518474801871ed
SargisVardanian <105112007+SargisVardanian@users.noreply.github.com>
2023-05-28 11:44:48 +0400

    Merge remote-tracking branch 'origin/main'

--- DenseNetwork.py
+++ DenseNetwork.py
@@ -24,25 +24,29 @@ class Neuron:
 
 
 class DenseLayer:
-    def __init__(self, neurons=64,  lr=0.01, max_iter=1000):
+    def __init__(self, neurons=64,  lr=0.01):
         self.neurons = neurons
         self.weights = None
         self.previous_layer = None
-        self.max_iter = max_iter
         self.lr = lr
 
     def forward(self, X, weights=None, flag=False):
         self.weights = np.random.randn(X.shape[1] + 1, self.neurons)
         print('self.weights', self.weights.shape)
-        X= np.insert(X, 0, 1, axis=1)
+        X = np.insert(X, 0, 1, axis=1)
         if flag:
             return X @ weights
         return X @ self.weights
 
     def backward(self, X, y, grad_outputs):
+        grad_outputs = np.array(grad_outputs)
+        print("grad_outputs.T, self.weights", grad_outputs.shape, self.weights.T.shape)
         grad = np.dot(grad_outputs, self.weights.T)
-        grad_weights = np.dot(X.T, grad_outputs)
-        self.weights -= self.lr * grad_weights
+        print("gg", grad.shape)
+        grad_weights = np.dot(grad_outputs.T, grad)
+        print('self.weights', self.weights.shape, grad_weights.shape)
+        self.weights -= self.lr * grad_weights.T
+        # self.weights[-1] -= self.lr * np.sum(grad_weights[-1], axis=0)
         return grad, self.weights
 
 
@@ -64,29 +68,29 @@ class DenseNetwork:
 
     def backward(self, X, y, grad_outputs):
         grad_inputs = grad_outputs
+        i = len(self.layers) - 1
         for layer in reversed(self.layers):
-            grad_inputs, weights = layer.backward(X, y, grad_inputs)
-        return grad_inputs, weights
+            print('layer', layer, i, np.array(grad_inputs[i]).shape)
+            grad_inputs, weights = layer.backward(X, y, grad_inputs[i])
+            i -= 1
+            if layer == self.layers[-1]:
+                grad_outputs = grad_inputs - y
+            else:
+                grad_outputs -= grad_inputs
+        return grad_outputs, weights
 
     def fit(self, X, y):
-        weights = []
         y = y.reshape(-1, 1)  # Reshape y to match the shape of outputs
         input = self.forward(X)
-        inp_ar = [input]
         for _ in range(self.max_iter):
             # for _ in self.layers:
             input = self.forward(input)
             print('input', input.shape)
             inp_ar = self.inp_ar
             # print('inp_ar', inp_ar[1])
-            for i in reversed(self.layers):
-                output = self.backward(X, y, inp_ar[i])
-                if i == self.layers[-1]:
-                    grad_outputs = output - y
-                else:
-                    grad_outputs -= output
-                weights.append(self.backward(X, y, grad_outputs)[1])
-            self.weights = weights
+            output, weights = self.backward(X, y, inp_ar)
+            weights.append(self.backward(X, y, output)[1])
+        self.weights = weights
     def predict(self, X):
         return self.forward(X, self.weights, flag=True)
 
