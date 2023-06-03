67d5ddb4c16ce5ec743e45ae00d3ef221f671d5c
SargisVardanian <105112007+SargisVardanian@users.noreply.github.com>
2023-06-04 01:17:07 +0400

    Merge remote-tracking branch 'origin/main'

---
+++ ADAM_Dense_Network.py
@@ -0,0 +1,142 @@
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import numpy as np
+import pickle
+
+X, y = make_regression(n_samples=100, n_features=10, noise=0.5, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+y_train = y_train.reshape(-1, 1)
+y_test = y_test.reshape(-1, 1)
+
+
+
+class DenseLayer:
+    def __init__(self, input_size, output_size, activation='relu'):
+        self.weights = np.random.randn(input_size, output_size)
+        self.biases = np.ones(output_size)
+        self.activation = activation
+        self.m_w = np.zeros_like(self.weights)
+        self.v_w = np.zeros_like(self.weights)
+        self.m_b = np.zeros_like(self.biases)
+        self.v_b = np.zeros_like(self.biases)
+        self.beta1 = 0.9
+        self.beta2 = 0.999
+        self.epsilon = 1e-8
+        self.t = 0
+
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.dot(inputs, self.weights) + self.biases
+        if self.activation == 'relu':
+            self.output = self.relu(self.output)
+            print('self.output', self.output.shape)
+        return self.output
+
+    def backward(self, grad_output, learning_rate):
+        if self.activation == 'relu':
+            grad_output = self.def_relu(self.output) * grad_output
+
+            grad_weights = np.dot(self.inputs.T, grad_output)
+            grad_biases = np.sum(grad_output, axis=0)
+
+            grad_weights = self.def_relu(grad_weights)
+            grad_biases = self.def_relu(grad_biases)
+        else:
+            grad_weights = np.dot(self.inputs.T, grad_output)
+            grad_biases = np.sum(grad_output, axis=0)
+
+
+        self.t += 1
+        self.m_w = self.beta1 * self.m_w + (1 - self.beta1) * grad_weights
+        self.v_w = self.beta2 * self.v_w + (1 - self.beta2) * (grad_weights ** 2)
+        m_w_hat = self.m_w / (1 - self.beta1 ** self.t)
+        v_w_hat = self.v_w / (1 - self.beta2 ** self.t)
+        self.weights -= learning_rate * m_w_hat / (np.sqrt(v_w_hat) + self.epsilon)
+
+        self.m_b = self.beta1 * self.m_b + (1 - self.beta1) * grad_biases
+        self.v_b = self.beta2 * self.v_b + (1 - self.beta2) * (grad_biases ** 2)
+        m_b_hat = self.m_b / (1 - self.beta1 ** self.t)
+        v_b_hat = self.v_b / (1 - self.beta2 ** self.t)
+        self.biases -= learning_rate * m_b_hat / (np.sqrt(v_b_hat) + self.epsilon)
+
+        grad_input = np.dot(grad_output, self.weights.T)
+        return grad_input
+
+    def relu(self, layer):
+        # print('np.maximum(0.0, layer)', np.maximum(0.0, layer))
+        return np.maximum(0.0, layer)
+
+    def def_relu(self, layer):
+        return np.where(layer > 0, 1, np.finfo(float).eps)
+
+        # return np.where(layer > 0, 1, 0)
+
+
+class DenseNetwork:
+    def __init__(self):
+        self.layers = []
+
+    def add_layer(self, layer):
+        self.layers.append(layer)
+
+    def forward(self, inputs):
+        for layer in self.layers:
+            inputs = layer.forward(inputs)
+        return inputs
+
+    def backward(self, grad_output, learning_rate):
+        for layer in reversed(self.layers):
+            grad_output = layer.backward(grad_output, learning_rate)
+
+    def fit(self, X_train, y_train, learning_rate, num_epochs):
+        for epoch in range(num_epochs):
+            y_pred = self.forward(X_train)
+            grad_output = 2 * (y_pred - y_train) / len(X_train)
+            self.backward(grad_output, learning_rate)
+
+            loss = np.abs(np.sum(y_pred - y_train) / len(X_train))
+            print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {loss}')
+
+    def loss(self, X_test, y_test):
+        y_pred = self.forward(X_test)
+        loss = np.abs(np.sum(y_pred - y_test) / len(y_test))
+        return loss
+
+    def save_model(self, file_path):
+        model_params = {
+            'layers': self.layers
+        }
+        with open(file_path, 'wb') as f:
+            pickle.dump(model_params, f)
+
+    @staticmethod
+    def load_model(file_path):
+        with open(file_path, 'rb') as f:
+            model_params = pickle.load(f)
+        model = DenseNetwork()
+        model.layers = model_params['layers']
+        return model
+
+
+# Train the DenseNetwork implemented from scratch
+dense_net = DenseNetwork()
+
+dense_net.add_layer(DenseLayer(10, 10, activation='relu'))
+dense_net.add_layer(DenseLayer(10, 1, activation='relu'))
+
+# Train the DenseNetwork using gradient descent
+learning_rate = 0.0008
+num_epochs = 10000
+
+dense_net.fit(X_train, y_train, learning_rate, num_epochs)
+#
+dense_net.save_model('trained_model_ADAM.pkl')
+
+# loaded_model = DenseNetwork.load_model('trained_model.pkl')
+# y_pred_dense = loaded_model.forward(X_test)
+
+y_pred_dense = dense_net.forward(X_test)
+
+# Compare the results
+print("Mean Squared Error (DenseNetwork implemented from scratch):", dense_net.loss(X_test, y_test))
---
+++ tensorflow_hospital_week12.py
@@ -0,0 +1,112 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import pickle
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+dataset = pd.read_csv("new_data.csv")
+
+X = dataset.drop('In-hospital_death', axis=1)
+y = dataset['In-hospital_death']
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+
+
+class DenseLayer(tf.Module):
+    def __init__(self, input_dim, output_dim):
+        super(DenseLayer, self).__init__()
+        self.W = tf.Variable(tf.random.normal([input_dim, output_dim], stddev=0.1))
+        self.b = tf.Variable(tf.random.normal([output_dim], stddev=0.1))
+        print("self.W, self.b0", self.W.shape, self.b.shape)
+
+    def __call__(self, x):
+        print("self.W, self.b1", self.W.shape, self.b.shape)
+        return tf.nn.relu(tf.matmul(x, self.W) + self.b)
+
+class SurvivalModel(tf.Module):
+    def __init__(self):
+        super(SurvivalModel, self).__init__()
+        self.layers = []
+
+    def add_layer(self, input_dim, output_dim):
+        self.layers.append(DenseLayer(input_dim, output_dim))
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+            # print('layer(x)', x)
+        return x
+
+    def loss_fn(self, y_true, y_pred):
+        return tf.reduce_mean(tf.square(y_true - y_pred))
+
+    @tf.function
+    def train_step(self, inputs, targets, optimizer, loss_fn):
+        with tf.GradientTape() as tape:
+            predictions = self(inputs)
+            loss = loss_fn(targets, predictions)
+        if tf.math.is_nan(loss):
+            return 0.0
+        gradients = tape.gradient(loss, self.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+        return loss
+
+    def train(self, X_train, y_train, num_epochs, batch_size, optimizer, loss_fn):
+        num_batches = len(X_train) // batch_size
+        for epoch in range(num_epochs):
+            epoch_loss = 0.0
+            for batch in range(num_batches):
+                start = batch * batch_size
+                end = start + batch_size
+                batch_inputs = tf.convert_to_tensor(X_train[start:end], dtype=tf.float32)
+                batch_targets = tf.convert_to_tensor(y_train[start:end], dtype=tf.float32)
+                # print('batch_inputs,batch_targets', batch_inputs.shape, batch_targets)
+                loss = self.train_step(batch_inputs, batch_targets, optimizer, loss_fn)
+                # print('loss', loss.numpy())
+                epoch_loss += loss
+            if (epoch + 1) % 100 == 0:
+                print('Epoch {}/{} - Loss: {:.4f}'.format(epoch + 1, num_epochs, epoch_loss / num_batches))
+
+    def save_model(self, file_path):
+        model_params = {
+            'layers': self.layers
+        }
+        with open(file_path, 'wb') as f:
+            pickle.dump(model_params, f)
+
+    @staticmethod
+    def load_model(file_path):
+        with open(file_path, 'rb') as f:
+            model_params = pickle.load(f)
+        model = SurvivalModel()
+        model.layers = model_params['layers']
+        return model
+
+
+model = SurvivalModel()
+input_dim = X_train.shape[1]  # Get the number of input features
+model.add_layer(input_dim=input_dim, output_dim=8)
+model.add_layer(input_dim=8, output_dim=4)
+model.add_layer(input_dim=4, output_dim=8)
+model.add_layer(input_dim=8, output_dim=1)
+optimizer = tf.optimizers.Adam(learning_rate=0.0001)
+
+
+print('DenseLayer', DenseLayer(input_dim=input_dim, output_dim=1))
+
+num_epochs = 1200
+batch_size = 40
+model.train(X_train, y_train, num_epochs, batch_size, optimizer, model.loss_fn)
+
+model.save_model("saved_model_week12.pkl")
+# model = SurvivalModel.load_model("saved_model_week12.pkl")
+
+
+X_test = tf.convert_to_tensor(X_test, dtype=tf.float32)
+y_test = tf.convert_to_tensor(y_test, dtype=tf.float32)
+predictions = model(X_test)
+
+print('Survival probability:', model.loss_fn(y_test, predictions))
